fcd4fba0b9771905e0d61c36c0bb10cb43de6428
addymwenda12 <muneneaddy@gmail.com>
2024-08-17 17:44:25 +0300

    chore: Update puppeteerScraper to use custom user agent

--- server/scrapers/puppeteerScraper.ts
+++ server/scrapers/puppeteerScraper.ts
@@ -6,7 +6,8 @@ export const puppeteerScraper = async (url: string): Promise<void> => {
   const page = await browser.newPage();
 
   try {
-    await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3');
+    const userAgent = process.env.USER_AGENT || 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3';
+    await page.setUserAgent(userAgent);
     await page.goto(url, { waitUntil: "networkidle2" });
 
     const title = await page.title();
@@ -15,7 +16,7 @@ export const puppeteerScraper = async (url: string): Promise<void> => {
     console.log("Title:", title);
     console.log("Description:", description);
 
-    await randomDelay(1000, 5000);
+    await randomDelay(Number(process.env.MIN_DELAY), Number(process.env.MAX_DELAY));
   } catch (error) {
     if (error instanceof Error) {
       console.error("Puppetter scraper error:", error.message);
