436144cbe110566250b56a7c139f72eb6adad1f2
Tracy Karanja <chirutracy@gmail.com>
2024-08-17 18:42:42 +0300

    add frontend code :sparkles:

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,69 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import axios from "axios"
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [url, setUrl] = useState("");
+  const [result, setResult] = useState<{
+    title: string;
+    url: string;
+  } | null>(null)
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault()
+    try{
+      const res = await axios.post("/api/scrape", {url})
+      setResult(res.data)
+    } catch(error) {
+      console.error(error)
+    }
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+    <div className="flex flex-col items-center justify-center">
+      <h1 className="text-4xl font-bold mb-8">Web scraper</h1>
+      <p className="mb-8 text-lg">
+        Please enter a URL, and we'll help you scrape the title of the webpage
+        for easy viewing. Simply paste the link and let us do the rest!
       </p>
-    </>
-  )
+      <form
+        onSubmit={handleSubmit}
+        className="rounded-lg p-6 w-96 border border-customLime"
+      >
+        <input
+          type="text"
+          value={url}
+          onChange={(e) => setUrl(e.target.value)}
+          className="w-full p-2 my-2 border border-customLime rounded-lg"
+        />
+        <button
+          type="submit"
+          className="border border-customLime text-customLime p-2 rounded-lg"
+        >
+          Scrape
+        </button>
+      </form>
+      {result && (
+        <div className="shadow-md rounded-lg p-6 mt-8 w-96">
+          <h2 className="text-xl font-semibold mb-4">Scraped Data</h2>
+          <table className="table-auto w-full border-collapse">
+            <tbody>
+              <tr className="border-b border-customLime">
+                <td className="font-semibold p-2">Title</td>
+                <td className="p-2">{result.title}</td>
+              </tr>
+              <tr>
+                <td className="font-semibold p-2 border-b border-customLime">
+                  URL
+                </td>
+                <td className="p-2 border-b border-customLime">{result.url}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default App
